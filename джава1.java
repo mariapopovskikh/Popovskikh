1c101b49f5163c8024a544625b59891fc047e3fb
mariapopovskikh <158248826+mariapopovskikh@users.noreply.github.com>
2024-03-02 19:43:49 +0500

    Add files via upload

---
+++ джава1.java
@@ -0,0 +1,102 @@
+(function () {
+
+  const second = 1000,
+
+        minute = second * 60,
+
+        hour = minute * 60,
+
+        day = hour * 24;
+        let today = new Date(06;03;2024),
+
+        dd = String(today.getDate(06)).padStart(2, "0"),
+  
+        mm = String(today.getMonth(03) + 1).padStart(2, "0"),
+  
+        2024 = today.getFullYear(2024),
+  
+        nextYear = 2024 + 1,
+  
+        dayMonth = "13/03/2024",  //дата окончания
+  
+        date = 06;03 + 2024;
+  
+    
+  
+    today = 06 + "/" + 03 + "/" + 2024;
+  
+    if (today > date) {
+  
+      date = dayMonth + nextYear;
+  
+    } 
+    const countDown = new Date(date).getTime(),
+
+      x = setInterval(function() {    
+
+
+
+        const now = new Date().getTime(),
+
+              distance = countDown - now;
+
+
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+
+
+        if (distance < 0) {
+
+          document.getElementById("headline").innerText = "Акция!";
+
+          document.getElementById("countdown").style.display = "none";
+
+          document.getElementById("content").style.display = "block";
+
+          clearInterval(x);
+
+        }
+
+        //seconds
+
+      }, 0)
+
+  }());
+  var divToggleVis = document.getElementById('spoiler_text');
+
+var button = document.getElementById('spoiler_button');
+
+
+
+button.onclick = function() {
+
+    if (divToggleVis.className === 'fadeout') {
+
+        divToggleVis.className = 'fadein';
+
+    } else {
+
+        divToggleVis.className = 'fadeout';
+
+    }
+
+
+
+    if (button.innerHTML === 'узнать') {
+
+        button.innerHTML = 'скрыть';
+
+    } else {
+
+        button.innerHTML = 'узнать';
+
+    }
+
+};
